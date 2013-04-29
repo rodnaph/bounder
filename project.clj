4c13d69c287dc793180c223e09ef74084355daae
rod <rod@pu-gh.com>
2013-04-29 19:05:44 +0100

    added cljsbuild

--- project.clj
+++ project.clj
@@ -1,11 +1,24 @@
 
 (defproject bounder "0.1.0"
   :description "Clojure and ClojureScript Docs"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [; clojure
+                 [org.clojure/clojure "1.5.1"]
                  [clj-jgit "0.3.5"]
                  [codox "0.6.4"]
                  [fs "1.3.3"]
-                 [enlive "1.0.1"]]
-  :source-paths ["src" "resources/views"]
-  :main bounder.core)
+                 [enlive "1.0.1"]
+                 ; clojurescript
+                 [enfocus "1.0.1"]]
+  :source-paths ["src/clojure" "resources/views"]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :main bounder.core
+  :cljsbuild {
+    :builds [{
+      :source-paths ["src/cljs"]
+      :compiler {:output-to "assets/js/main.js"
+                 :optimizations :whitespace
+                 :pretty-print true}
+    }]
+  }
+  :hooks [leiningen.cljsbuild])
 
